d9fe86c2562222e05c48a4ecd6426f0cea8201fc
treybreckenridge <46287855+treybreckenridge@users.noreply.github.com>
2019-01-06 23:36:40 -0600

    Created 404.php (Page Not Found) with a search box

---
+++ 404.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * The template for displaying 404 pages (Page Not Found)
+ *
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<div id="content" class="site-content" role="main">
+
+			<header class="page-header">
+				<h1 class="page-title"><?php _e( 'Page Not Found', 'cap-wp-theme-master' ); ?></h1>
+			</header>
+
+			<div class="page-wrapper">
+				<div class="page-content">
+				    
+					<h2><?php _e( 'Sorry, but the page you requested could not be found.', 'cap-wp-theme-master' ); ?></h2>
+					<p><?php _e( 'The page or file you are looking for does not exist, has been moved,', 'cap-wp-theme-master' ); ?><br>
+					<?php _e( 'or is no longer available.', 'cap-wp-theme-master' ); ?></p>
+					<p><?php _e( 'Maybe try a search?', 'cap-wp-theme-master' ); ?></p>
+					<?php get_search_form(); ?>
+				</div><!-- .page-content -->
+			</div><!-- .page-wrapper -->
+
+		</div><!-- #content -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
